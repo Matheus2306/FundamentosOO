b0bd85a7307c164aa10c4e856c64e1ec51f367c2
Matheus2306 <Math2306.r@gmail.com>
2024-08-21 08:54:24 -0300

    add class Cavaleiro

---
+++ FundamentosOO/Cavaleiro.cs
@@ -0,0 +1,18 @@
+﻿using Fundamentos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundamentosOO
+{
+    internal class Cavaleiro : Personagem
+    {
+        public override void apresentarSe()
+        {
+            Console.WriteLine("Olá, eu sou o Mago " + this.Nome + " tenho " + this.Vida + " pontos de vida.\nInteligência: " + this.Inteligencia + "\nAgilidade: " + this.Agilidade + "\nForça: " + this.Forca + "\nNível atual é: " + this.Nivel);
+            Console.WriteLine("==================================================");
+        }
+    }
+}
